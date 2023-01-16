6214ce50da5489117ab1702d3bc1f29d510c0b6f
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2023-01-16 14:31:19 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -24,7 +24,7 @@ export const getAccessToken = async () => {
   return accessToken;
 };
 
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
@@ -39,6 +39,11 @@ export const getEvents = async () => {
   if (window.location.href.startsWith("http://localhost")) {
     NProgress.done();
     return mockData;
+  }
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];
   }
 
   const token = await getAccessToken();
